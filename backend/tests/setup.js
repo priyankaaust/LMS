23115595413cb79d55eb58728f25bc6501c52578
root <root@DESKTOP-T1LKODV.>
2025-07-14 01:28:03 -0230

    unit testing added for backend

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
-// const { MongoMemoryServer } = require('mongodb-memory-server');
 const express = require('express');
 
 let mongo;
 
 beforeAll(async () => {
-  // mongo = await MongoMemoryServer.create();
-  // const uri = mongo.getUri();
-  // console.log(process.env.MONGO_URI);
+
   await mongoose.connect(process.env.MONGO_URI);
 
 }, 20000); // optional: allow 20s for slow systems
